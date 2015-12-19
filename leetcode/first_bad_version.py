67ae479eda067c85c53e3167c6284f932f807ef3
Nate Willard <nate.a.willard@gmail.com>
2015-12-19 14:16:31 -0800

    start of first bad version

---
+++ leetcode/first_bad_version.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/first-bad-version/
+#  You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+# 
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+# 
+# You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API. 
+
+
+# Seems like a binary search problem
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        def helper(leftpos, rightpos):
+            midpoint = math.floor((leftpos + rightpos) / 2)
+            if isBadVersion(midpoint)
+
+        return helper(0, n-1)
+        """
+        :type n: int
+        :rtype: int
+        """
+        
