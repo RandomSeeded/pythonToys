9348a91eed39b5fe87ed179ff26ecf486a13345d
Nate Willard <nate.a.willard@gmail.com>
2015-12-22 08:04:31 -0800

    Finish add digits

--- leetcode/add_digits.py
+++ leetcode/add_digits.py
@@ -8,6 +8,13 @@
 # Follow up:
 # Could you do it without any loop/recursion in O(1) runtime? 
 
+# HARD VERSION: constant runtime, not even a loop?
+class Solution:
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        remainder = num % 9
+        return remainder if remainder != 0 else 9
 
 # EASY VERSION (recursion)
 class Solution:
@@ -23,3 +30,14 @@ class Solution:
 
 sol = Solution()
 print(sol.addDigits(38))
+
+# POSSIBLE RESULTS: not 0-9, actually they're 1-9
+# Can we easily check? Aka just loop through 1-9 and check each of these?
+# Well, sort of 0-9. Anyway, can we check constant time on that?
+
+# How can we check if a solution is correct besides adding em up?
+# OK, googling shows it's related to 9s. 38 % 9 = 2. Huh
+# 692 % 9 = 8
+# 692 : 17: 8
+# who knew? I did not.
+
