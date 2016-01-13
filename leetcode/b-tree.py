807bdcbec40be62afe1b9bf902855acbe0adaf9a
Nate Willard <nate.a.willard@gmail.com>
2016-01-12 19:25:50 -0800

    Add basic insert structure

--- leetcode/b-tree.py
+++ leetcode/b-tree.py
@@ -1,6 +1,7 @@
 class Node:
     def __repr__(self):
-        return str(self.values) + " with children: "+str(self.children)
+        return str(self.values) + " with children: "+ str(self.children)
+
     def __init__(self, isLeaf = True, numElements = 0, parent = None):
         self.values = []
         self.children = []
@@ -12,16 +13,46 @@ class Node:
 class BTree:
     def __repr__(self):
         return str(self.root)
+
     def __init__(self):
         self.root = Node()
         self.n = 0
 
+    def search(self, val, currentNode = None):
+        """
+        Returns the node where a value should be placed
+        :rtype: Node
+        """
+        if currentNode == None:
+            currentNode = self.root
+
+        if currentNode.isLeaf or val in currentNode:
+            return currentNode
+        else:
+            for i in range(len(self.values)):
+                if val < self.values[i]:
+                    return self.search(val, self.children[i])
+            return self.search(val, self.children[i])
+
     def insert(self, val):
-        pass
+        location = self.search(val)
+        if location.numElements < 3:
+            location.values.append(val)
+            location.numElements+=1
+        elif location.parent == None:
+            pass
+        elif location.parent.numElements < 3:
+            pass
+        else:
+            pass
 
 
 myTree = BTree()
 
 myTree.insert(5)
+myTree.insert(4)
+myTree.insert(3)
+myTree.insert(2)
+myTree.insert(1)
 print(myTree)
 
