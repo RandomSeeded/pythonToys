ac422445b0a4a1f16ffff1644c10b87d5d62d0f6
Nate Willard <nate.a.willard@gmail.com>
2016-01-20 15:59:12 -0800

    Add correct (but slow) solution

--- leetcode/es6/countPrimes.js
+++ leetcode/es6/countPrimes.js
@@ -13,32 +13,54 @@
  */
 
 // ES5
+// This time actually solving the problem being asked. How many primes BELOW a certain #?
+// AKA 
 var countPrimes = function(n) {
-  var seen = {};
-
-  var helperPrimes = function(n, results) {
-    results = results || 0;
-    if (seen[n]) {
-      return seen[n];
-    }
-    var isPrime = true;
+  function isPrime(n) {
     for (var i = 2; i <= Math.floor(n/2); i++) {
-      var remainder = n % i;
-      if (remainder === 0) {
-        results += helperPrimes(n / i);
-        isPrime = false;
+      if (n % i === 0) {
+        return false;
       }
     }
-    if (isPrime) {
-      results++;
-    }
-    seen[n] = results;
-    return results;
+    return true;
   }
 
-  return helperPrimes(n);
+  var results = 0;
+  for (var i = 2; i < n; i++) {
+    if (isPrime(i)) {
+      results++;
+    }
+  }
+  return results;
 }
 
 console.log(countPrimes(7));
 console.log(countPrimes(8));
 console.log(countPrimes(16));
+
+// var countPrimes = function(n) {
+//   var seen = {};
+// 
+//   var helperPrimes = function(n, results) {
+//     results = results || 0;
+//     if (seen[n]) {
+//       return seen[n];
+//     }
+//     var isPrime = true;
+//     for (var i = 2; i <= Math.floor(n/2); i++) {
+//       var remainder = n % i;
+//       if (remainder === 0) {
+//         results += helperPrimes(n / i);
+//         isPrime = false;
+//       }
+//     }
+//     if (isPrime) {
+//       results++;
+//     }
+//     seen[n] = results;
+//     return results;
+//   }
+// 
+//   return helperPrimes(n);
+// }
+
