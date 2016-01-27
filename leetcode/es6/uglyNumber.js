7eddd6db04c206d0636863257b01ef34338596b2
Nate Willard <nate.a.willard@gmail.com>
2016-01-27 13:39:06 -0800

    Add faster solution to ugly number

--- leetcode/es6/uglyNumber.js
+++ leetcode/es6/uglyNumber.js
@@ -10,34 +10,67 @@
  * @param {number} num
  * @return {boolean}
  */
+
+// New approach: remove all factors 2, 3, and 5, then see if there are any other prime factors
 var isUgly = function(num) {
   var uglyFactors = {
     2: true,
     3: true,
     5: true
-  }
+  };
 
-  var hasNonUglyFactor = function(num) {
-    var isPrime = true;
-    for (var i = 2; i < num; i++) {
-      if (num % i === 0) {
-        isPrime = false;
-        if (hasNonUglyFactor(num / i)) {
-          return true;
-        }
-      }
-    }
-    if (isPrime && !uglyFactors[num]) { 
-      return true;
-    }
+  // If all factors were ugly, it's ugly
+  if (num === 1) {
+    return true;
+  } else if (num === 0) {
     return false;
   }
 
-  return ! hasNonUglyFactor(num); 
-}
+  for (var factor in uglyFactors) {
+    // If there are any ugly factors, remove and recurse
+    if (num % factor === 0) {
+      return isUgly(num / factor);
+    } 
+  }
+  // Otherwise, we have a non-ugly number
+  return false;
+};
 
+// This version works (I believe) but is too slow
+// var isUgly = function(num) {
+//   var uglyFactors = {
+//     2: true,
+//     3: true,
+//     5: true
+//   }
+// 
+//   var hasNonUglyFactor = function(num) {
+//     var isPrime = true;
+//     for (var i = 2; i <= Math.sqrt(num); i++) {
+//       if (num % i === 0) {
+//         isPrime = false;
+//         if (hasNonUglyFactor(num / i)) {
+//           return true;
+//         }
+//       }
+//     }
+//     if (isPrime && !uglyFactors[num]) { 
+//       return true;
+//     }
+//     return false;
+//   }
+// 
+//   return ! hasNonUglyFactor(num); 
+// }
+
+console.log(isUgly(4));
 console.log(isUgly(10));
+console.log(isUgly(20));
+console.log(isUgly(14));
 console.log(isUgly(70));
+console.log(isUgly(905391974));
+console.log(isUgly(2123366400));
+
 
 
 
